1caa93567ca40ce278d1195a900c4a794924b8fc
Mitsobabis <91488150+Mitsobabis@users.noreply.github.com>
2022-06-16 20:50:49 +0300

    Add files via upload
    Code is still retarded but now I've not only included JOptionPanes. I added JtextFields too :)

--- password gen/src/redirect.java
+++ password gen/src/redirect.java
@@ -1,17 +1,45 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class redirect {
+public class redirect extends JFrame implements ActionListener {
+    JButton button;
+    JTextField textField;
     redirect(){
         String password = Main.getRngPassword();
         System.out.println("----------------------------------------------------------\n" + password + "\n----------------------------------------------------------");
-        JOptionPane.showInternalMessageDialog(null, password);
-        String question = JOptionPane.showInputDialog("Do you want to generate more passwords?\nType 'Yes' to generate a new one, or anything else to exit!");
-        if(question == null){
-            System.exit(0);
-        }else if(question.equals("Yes")){
-            new redirect();
-        }else{
-            System.exit(0);
+        JOptionPane.showMessageDialog(null, "Your Password is successfully saved in your Clipboard");
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        button = new JButton("Submit");
+        button.addActionListener(this);
+
+        textField = new JTextField();
+        textField.setPreferredSize(new Dimension(250,40));
+
+        this.add(button);
+        this.add(textField);
+        this.setLocation(400,400);
+        this.setTitle("Do you want to generate more random passwords?");
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == button){
+           String text = textField.getText();
+            if(text.equals("Yes")){
+                this.setVisible(false);
+                new redirect();
+            }
+            else{
+                System.out.println("[LOG] Shutting Down...");
+                System.exit(0);
+            }
         }
     }
 }
